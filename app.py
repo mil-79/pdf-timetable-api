b13308c5c9c03c573c3c7590789ee6792cf0594a
mil-79 <s423004@mailg.shumei-u.ac.jp>
2026-05-01 16:53:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def pdf():
 
         with pdfplumber.open(file.stream) as pdf:
             for i, page in enumerate(pdf.pages):
-                if i >= 20:
+                if i >= 18:
                     break
 
                 text = page.extract_text()
